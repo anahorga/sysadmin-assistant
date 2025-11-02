c66067a3acf05e4f110933e1567fffac4bb1541b
anahorga <ana.horga9@gmail.com>
2025-11-02 13:24:00 +0200

    finished implementing the agent and started the documantation

--- my_agent/agent.py
+++ my_agent/agent.py
@@ -4,15 +4,21 @@ from google.adk.tools.mcp_tool.mcp_toolset import MCPToolset
 from google.adk.tools.mcp_tool.mcp_session_manager import StreamableHTTPConnectionParams
 
 root_agent = Agent(
-    model=LiteLlm(model="ollama_chat/llama3.2"),
+    model=LiteLlm(model="ollama_chat/llama3.2",allow_tools=False),
     name="My_Sysadmin",
     description=(
         "hello world, i'm an agent that can do some sys admin stuff for you"
         
     ),
-    instruction="""
-      You ask me something about folders and i show you.
-    """,
+instruction = """
+You are My_Sysadmin - a friendly chat assistant who can also do light sysadmin tasks.
+
+DO NOT use any tools unless the input message explicitly contains one of the following action words:
+["list", "ls", "dir", "show contents", "open file", "read file", "view file", "what's in this folder", "path", "directory", "file"]
+
+If unsure, respond conversationally and do not call any tools unless instructed clearly.
+""",
+
     tools=[
         MCPToolset(
             connection_params=StreamableHTTPConnectionParams(
